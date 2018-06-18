eb716c0e9824e5e7e32b39347f075f8846c1e256
Le Duy <duyle3007@gmail.com>
2018-06-18 11:42:09 +0700

    First Demo of image button

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: 'http://facebook.github.io/react-native/img/favicon.png'};
+  }
+
+  export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
+          <Image source={{uri: "http://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+        </TouchableOpacity>
+        <Text>We are just getting started</Text>
       </View>
     );
   }
@@ -16,5 +26,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
   },
 });
